aca30fec1cc3996506f646b2134e21d95fcd4265
pavlvs <pavlvsxavier@gmail.com>
2021-09-22 10:10:01 +0100

    implementing breadth first search

---
+++ runner.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+class Runner {
+    constructor() {
+        this.files = []
+    }
+    async collectFiles(targetPath) {
+        const files = await fs.promises.readdir(targetPath)
+        return files
+    }
+}
+
+module.exports = Runner
